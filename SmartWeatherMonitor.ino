06d6e564bea15f25ed9b2ad39f89507bc9115b68
Nirmalaaiswarya <36539453+Nirmalaaiswarya@users.noreply.github.com>
2019-05-22 15:56:34 +0530

    Add files via upload

--- SmartWeatherMonitor.ino
+++ SmartWeatherMonitor.ino
@@ -4,7 +4,7 @@
 //-------- Customise these values -----------
 const char* ssid = "Redmi";
 const char* password = "easycall";
-//String command;
+
 #include "DHT.h"
 #define DHTPIN D2    // what pin we're connected to
 #define DHTTYPE DHT11   // define type of sensor DHT 11
@@ -15,19 +15,24 @@ DHT dht (DHTPIN, DHTTYPE);
 #define DEVICE_ID "BHAVANI26"
 #define TOKEN "Bh@v@365"
 //-------- Customise the above values --------
- 
+ String command;
 char server[] = ORG ".messaging.internetofthings.ibmcloud.com";
 char topic[] = "iot-2/evt/Data/fmt/json";
+char topic1[] = "iot-2/cmd/home/fmt/String";
 char authMethod[] = "use-token-auth";
 char token[] = TOKEN;
 char clientId[] = "d:" ORG ":" DEVICE_TYPE ":" DEVICE_ID;
  
 WiFiClient wifiClient;
-PubSubClient client(server, 1883,wifiClient);
+void callback(char* topic1, byte* payload, unsigned int payloadLength);
+PubSubClient client(server, 1883, callback, wifiClient);
 
 void setup() {
  Serial.begin(115200);
  Serial.println();
+  pinMode(D1,OUTPUT);
+  wifiConnect();
+  mqttConnect();
  dht.begin();
  Serial.print("Connecting to ");
  Serial.print(ssid);
@@ -43,7 +48,10 @@ void setup() {
 }
  
 void loop() {
-  
+  if (!client.loop()) {
+    mqttConnect();
+  }
+delay(100);
   
 float h = dht.readHumidity();
 float t = dht.readTemperature();
@@ -51,13 +59,10 @@ float t = dht.readTemperature();
 if (isnan(h) || isnan(t))
 {
 Serial.println("Failed to read from DHT sensor!");
-delay(1000);
+delay(100);
 return;
 }
 PublishData(t,h);
- if (!client.loop()) {
-    mqttConnect();
-  }
 delay(100);
 }
 void mqttConnect() {
@@ -66,17 +71,49 @@ void mqttConnect() {
     while (!client.connect(clientId, authMethod, token)) {
       Serial.print(".");
       delay(500);
-    }9
+    }
     initManagedDevice();
     Serial.println();
   }
 }
 void initManagedDevice() {
-  if (client.subscribe(topic)) {
+  if (client.subscribe(topic1)) {
     Serial.println("subscribe to cmd OK");
   } else {
     Serial.println("subscribe to cmd FAILED");
   }
+}
+
+void wifiConnect() {
+  Serial.print("Connecting to "); Serial.print(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("nWiFi connected, IP address: "); Serial.println(WiFi.localIP());
+}
+void callback(char* topic1, byte* payload, unsigned int payloadLength) {
+  Serial.print("callback invoked for topic: "); Serial.println(topic1);
+
+  for (int i = 0; i < payloadLength; i++) {
+    //Serial.println((char)payload[i]);
+    command += (char)payload[i];
+  }
+Serial.println(command);
+delay(1000);
+if(command == "LIGHTON"){
+  digitalWrite(D1,HIGH);
+ 
+  Serial.println("Light is Switched ON");
+}
+else if(command == "LIGHTOFF"){
+  digitalWrite(D1,LOW);
+
+  Serial.println("Light is Switched OFF");
+}
+delay(1000);
+command ="";
 }
 
 void PublishData(float temp, float humid){
